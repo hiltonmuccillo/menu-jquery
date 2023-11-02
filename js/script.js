9763a4adb2a49babbcbea7cb8aa46cbd59dce6fb
Hilton Muccillo <hmuccillo@gmail.com>
2023-11-02 18:24:13 -0300

    Upload menu jquery

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+  $("ul.content").hide();
+  $('ul.navigation li.toggle').bind("click", function(){
+    $(this).toggleClass("bg-blue");
+    $(this).siblings().removeClass("bg-blue");
+    $(this).find(".content").slideDown('fast');    
+    if ($('ul.navigation li.toggle').hasClass("bg-blue")) {
+      $("ul.navigation li.toggle").not(this).find(".content").slideUp('fast');
+    } else {
+      $("ul.navigation li.toggle").find(".content").slideUp('fast')
+    }  
+  });
+});
+  
